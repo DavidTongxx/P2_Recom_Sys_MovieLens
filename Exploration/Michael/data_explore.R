cd0c9bb422d03835c7b83931818317efe983f585
Zhuangye Chen <zhuangye@stat.wisc.edu>
2016-02-29 17:23:52 -0600

    Explore the variable "occupation"

--- Exploration/Michael/data_explore.R
+++ Exploration/Michael/data_explore.R
@@ -4,5 +4,7 @@ ML_df <- as(MovieLense, "data.frame")
 ML_mat <- as(MovieLense, "matrix")
 #head(ML_df)
 
-user <- read.table("./Data/ml-100k/u.user", sep = "|")
+user <- read.table("./Data/ml-100k/u.user", sep = "|", 
+                   colClasses = c("character", "integer", "factor", "character", "character"),
+                   col.names = c("user_id", "age", "gender", "occupation", "zipcode"))
 
