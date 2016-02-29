373126f38142fbda2d66045140f96504daee235a
Zhuangye Chen <zhuangye@stat.wisc.edu>
2016-02-29 15:38:45 -0600

    first commit

--- Exploration/Michael/data_explore.R
+++ Exploration/Michael/data_explore.R
@@ -1,2 +1,8 @@
 library("recommenderlab")
 data(MovieLense)
+ML_df <- as(MovieLense, "data.frame")
+ML_mat <- as(MovieLense, "matrix")
+#head(ML_df)
+
+user <- read.table("./Data/ml-100k/u.user", sep = "|")
+
