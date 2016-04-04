ea57c6d23c8068ea75367c90a4583176da4c7ccd
Zhuangye Chen <zhuangye@stat.wisc.edu>
2016-04-04 18:27:59 -0500

    change the affinity_mat function to return a list of 3 affinity matrix for each column

--- Exploration/Michael/affinity_matrix.R
+++ Exploration/Michael/affinity_matrix.R
@@ -32,25 +32,40 @@ user$occupation <- trans_ordinal(user$occupation)
 user$zipcode <- NULL
 
 # A function to calculate the affinity matrix
+# the output will be a list of 3 affinity matrix for each column
 affinity_mat <- function(user_df) { # user_df is the data frame of user info.
+  # normalize the data frame
+  user_df <- scale(user_df)
+  
   n <- dim(user_df)[1] # number of users
-  aff_mat <- matrix(numeric(n*n), nrow = n)
+  
+  aff_age <- matrix(numeric(n*n), nrow = n)
+  aff_gender <- matrix(numeric(n*n), nrow = n)
+  aff_occupation <- matrix(numeric(n*n), nrow = n)
   # set the diagonal element to 1
-  diag(aff_mat) <- 1
+  diag(aff_age) <- 1
+  diag(aff_gender) <- 1
+  diag(aff_occupation) <- 1
   
   # a vector to store the variance of each user
-  var_user <- sapply(1:n, FUN = function(x) sum((user[x, ] - mean(as.numeric(user[x, ])))^2))
+  var_user <- sapply(1:n, FUN = function(x) sum((user_df[x, ] - mean(as.numeric(user_df[x, ])))^2))
+  # a vector to store the mean of each user
+  mean_user <- rowMeans(user_df)
   
   for (i in seq_len(n)) {
     if (i == n) break
     for (j in seq(i+1, n)) {
-      nume <- sum((user[i, ] - mean(as.numeric(user[i, ]))) * (user[j, ] - mean(as.numeric(user[j, ]))))
-      denum <- sqrt(var_user[i] * var_user[j])
-      aff_mat[i, j] <- nume/denum
+      age <- (user_df[i, 'age']-mean_user[i]) * (user_df[j, 'age'] - mean_user[j])
+      gender <- (user_df[i, 'gender']-mean_user[i]) * (user_df[j, 'gender'] - mean_user[j])
+      occu <- (user_df[i, 'occupation']-mean_user[i]) * (user_df[j, 'occupation'] - mean_user[j])
+      aff_age[i, j] <- age/sqrt(var_user[i]*var_user[j])
+      aff_age[i, j] <- gender/sqrt(var_user[i]*var_user[j])
+      aff_occupation[i, j] <- occu/sqrt(var_user[i]*var_user[j])
     }
   }
-  return(aff_mat)
+  output <- list(aff_age, aff_gender, aff_occupation)
+  return(output)
 }
 
-
-system.time(affinity_matrix <- affinity_mat(user[1:100, ]))
+# affinity_matrix is a list of affinity matrix for each column (age, sex, occupation)
+system.time(affinity_matrix <- affinity_mat(user))
